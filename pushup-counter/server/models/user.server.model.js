ccc95de49d1d7ca1f413f41aa1d7c383e8073a6b
Peter B Smith <peterbutlersmith@gmail.com>
2016-01-31 12:55:28 -0500

    adding bcrypt and implementing password hashing on user model

--- pushup-counter/server/models/user.server.model.js
+++ pushup-counter/server/models/user.server.model.js
@@ -1,5 +1,6 @@
-// require mongoose
+// require mongoose and bcrypt
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema   = mongoose.Schema;
 
 //Create 'UserSchema'
@@ -14,6 +15,16 @@ var UserSchema = new Schema({
     required: true
   }
 });
+
+// method for generating a bcrypted hash
+UserSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// method for checking is password is valid
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 //Create the 'User' model based on 'UserSchema'
 mongoose.model('User', UserSchema);
