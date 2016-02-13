7b60d5783da1034d9b9374563f76b6592a9429c3
Peter B Smith <peterbutlersmith@gmail.com>
2016-02-13 13:42:08 -0500

    fixing jobs controller owner_id error

--- shoveler/server/controllers/jobs.server.controller.js
+++ shoveler/server/controllers/jobs.server.controller.js
@@ -5,25 +5,24 @@ var Owner    = mongoose.model('Owner');
 exports.postJobs = function(req, res) {
   var newJob = new Job();
 
-  Owner.findOne({_id: req.body._id }, function(err, owner) {
+  Owner.findOne({_id: req.body.owner_id }, function(err, owner) {
     if (err) { throw err; }
 
     if (!owner) {
       res.status(403).send({
         description: 'forbidden'
       });
+    } else {
+      newJob.size = req.body.size;
+      newJob.price = req.body.price;
+      newJob.notes = req.body.notes;
+      newJob.owner_id = owner._id;
+
+      newJob.save(function(err) {
+        if (err) { throw err; }
+        res.status(201).send({ description: 'job created!' });
+      });
     }
-
-    newJob.size = req.body.size;
-    newJob.price = req.body.price;
-    newJob.notes = req.body.notes;
-    newJob.owner_id = owner._id;
-
-    newJob.save(function(err) {
-      if (err) { throw err; }
-      res.status(201).send({ description: 'job created!' });
-    });
-
   });
 };
 
